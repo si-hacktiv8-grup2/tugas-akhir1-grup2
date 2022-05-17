ebf3df19749f68e9b57feb622a2196a932f61554
angg07 <anggasyamsul70@gmail.com>
2022-05-17 11:38:48 +0700

    commit on 17-05-2022

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <?php include 'component/header.php';?>
-
+    <link rel="stylesheet" href="assets/css/styles_porto.css">
 
     <style>
         h1 {
@@ -24,10 +24,70 @@
         include 'component/navbar.php';        
     ?>
 
-     
-    <br><br><br><br><br>
 
-    <!-- Footer -->
+    <!-- Contact Start -->
+    <div class="container-xxl py-6">
+        <div class="container">
+            <div class="section-header text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <h1 class="display-5 mb-3">Contact Us</h1>
+                <p>
+                    Untuk informasi lebih lanjut, Hubungi Kami
+                </p>
+            </div>
+            <div class="row g-5 justify-content-center">
+                <div class="col-lg-5 col-md-12 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="bg-primary text-white d-flex flex-column justify-content-center h-100 p-5">
+                        <h5 class="text-white">Call Us</h5>
+                        <p class="mb-5"><i class="fa fa-phone-alt me-3"></i>(021) 8067 5787</p>
+                        <h5 class="text-white">Email Us</h5>
+                        <p class="mb-5"><i class="fa fa-envelope me-3"></i>halo@hacktiv8.ac.id</p>
+                        <h5 class="text-white">Office Address</h5>
+                        <p class="mb-5"><i class="fa fa-map-marker-alt me-3"></i>
+                            Jl. Sultan Iskandar Muda No.7
+                            Kebayoran Lama
+                            Jakarta Selatan, 12240
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-7 col-md-12 wow fadeInUp" data-wow-delay="0.5s">
+                    <form>
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="name" placeholder="Your Name">
+                                    <label for="name">Your Name</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="email" class="form-control" id="email" placeholder="Your Email">
+                                    <label for="email">Your Email</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="subject" placeholder="Subject">
+                                    <label for="subject">Subject</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 200px"></textarea>
+                                    <label for="message">Message</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary rounded-pill py-3 px-5" type="submit">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br>
+    
+        <!-- Footer -->
     <?php include 'component/footer.php' ?>
 
     <!-- script -->
