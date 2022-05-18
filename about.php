8dc75c206590970ce6c2a7bde158d4a3970c6c0c
R1CryOn <muhhridwan94@gmail.com>
2022-05-18 22:52:39 +0700

    Update about.php

--- about.php
+++ about.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <?php include 'component/header.php';?>
-
+    
 
     <style>
         h1 {
@@ -24,6 +24,25 @@
         include 'component/navbar.php';        
     ?>
 
+  <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="about-img position-relative overflow-hidden p-5 pe-0">
+                        <img class="img-fluid w-100" src="assets/images/best.jpg">
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                    <h1 class="display-5 mb-4">Best Tour and Travel Guide</h1>
+                    <p class="mb-4">Chill tour is a tour and travel service provider that is very suitable for all circles of society. With the best service you also get attractive prices that are very friendly in your pocket.</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Tour and Travel Guide</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>For all society</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Good Service and Price</p>
+                    <a class="btn btn-primary rounded-pill py-3 px-5 mt-3" href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
      
     <br><br><br><br><br>
 
