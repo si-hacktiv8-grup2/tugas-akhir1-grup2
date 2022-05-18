1fd7e9c5c1d47cf88ee6afd0eb82d717a169cabb
R1CryOn <muhhridwan94@gmail.com>
2022-05-18 22:56:28 +0700

    Update portofolio.php

--- portofolio.php
+++ portofolio.php
@@ -29,23 +29,40 @@
             <div class="container px-4 px-lg-5">
                 <!-- Featured Project Row-->
                 <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+<<<<<<< HEAD
                     <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/images/globe.png" alt="..." /></div>
                     <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
                             <h4>Chill Tour</h4>
                             <p class="text-black-50 mb-0">Chill Tour is a service company that provides tour packages in the best places around the world. This company was founded by 3 youths namely Angga, Ridwan, and Adik.</p>
+=======
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/bg-masthead.jpg" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Shoreline</h4>
+                            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                         </div>
                     </div>
                 </div>
                 <!-- Project One Row-->
                 <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+<<<<<<< HEAD
                     <div class="col-lg-6"><img class="img-fluid" src="assets/images/turing.jpg" alt="..." /></div>
+=======
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-01.jpg" alt="..." /></div>
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                     <div class="col-lg-6">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-left">
+<<<<<<< HEAD
                                     <h4 class="text-white">Tour Guide</h4>
                                     <p class="mb-0 text-white-50">Group Touring Package is our main product that we provide. In addition to providing group tour packages, we also have tour guides who will accompany customers on their vacation trips.</p>
+=======
+                                    <h4 class="text-white">Misty</h4>
+                                    <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                                     <hr class="d-none d-lg-block mb-0 ms-0" />
                                 </div>
                             </div>
@@ -54,13 +71,22 @@
                 </div>
                 <!-- Project Two Row-->
                 <div class="row gx-0 justify-content-center">
+<<<<<<< HEAD
                     <div class="col-lg-6"><img class="img-fluid" src="assets/images/Award.jpg" alt="..." /></div>
+=======
+                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                     <div class="col-lg-6 order-lg-first">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-right">
+<<<<<<< HEAD
                                     <h4 class="text-white">Best Travel Guide of The Year</h4>
                                     <p class="mb-0 text-white-50">In addition, we have also received many awards as the best travel and touring service provider company in 2021.</p>
+=======
+                                    <h4 class="text-white">Mountains</h4>
+                                    <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                                     <hr class="d-none d-lg-block mb-0 me-0" />
                                 </div>
                             </div>
