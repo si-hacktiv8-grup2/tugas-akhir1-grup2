c73710dbef014dffad4b9308927ef1571ea07496
R1CryOn <muhhridwan94@gmail.com>
2022-05-18 22:56:23 +0700

    Update blog.php

--- blog.php
+++ blog.php
@@ -26,13 +26,22 @@
 
     <!-- Page Header-->
       <div class="container">
+<<<<<<< HEAD
         <header class="masthead" style="background-image: url('assets/images/Blog1.jpg')">
+=======
+        <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
             <div class="container position-relative px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="site-heading">
+<<<<<<< HEAD
                             <h1>Chill Tour Blog</h1>
                             <span class="subheading">Blogspot for Chill.Tour Company</span>
+=======
+                            <h1>Clean Blog</h1>
+                            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                         </div>
                     </div>
                 </div>
@@ -47,6 +56,7 @@
                     <!-- Post preview-->
                     <div class="post-preview">
                         <a href="post.html">
+<<<<<<< HEAD
                             <h2 class="post-title">
                             People should explore every beautiful place around the world.</h2>
                             <h3 class="post-subtitle">
@@ -56,17 +66,34 @@
                             Posted by
                             <a href="#!">Chill One</a>
                             on May 1, 2022
+=======
+                            <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
+                            <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Start Bootstrap</a>
+                            on September 24, 2022
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                         </p>
                     </div>
                     <!-- Divider-->
                     <hr class="my-4" />
                     <!-- Post preview-->
                     <div class="post-preview">
+<<<<<<< HEAD
                         <a href="post.html"><h2 class="post-title">What's so interesting about a tour package?</h2></a>
                         <p class="post-meta">
                             Posted by
                             <a href="#!">Chill One</a>
                             on May 10, 2022
+=======
+                        <a href="post.html"><h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2></a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Start Bootstrap</a>
+                            on September 18, 2022
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                         </p>
                     </div>
                     <!-- Divider-->
@@ -74,6 +101,7 @@
                     <!-- Post preview-->
                     <div class="post-preview">
                         <a href="post.html">
+<<<<<<< HEAD
                             <h2 class="post-title">Have Impressions and questions about us?</h2>
                             <h3 class="post-subtitle">Please send comments and contact us available on the web page.</h3>
                         </a>
@@ -81,6 +109,15 @@
                             Posted by
                             <a href="#!">Start Chill One</a>
                             on May 19, 2022
+=======
+                            <h2 class="post-title">Science has not yet mastered prophecy</h2>
+                            <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Start Bootstrap</a>
+                            on August 24, 2022
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                         </p>
                     </div>
                     <!-- Divider-->
@@ -88,6 +125,7 @@
                     <!-- Post preview-->
                     <div class="post-preview">
                         <a href="post.html">
+<<<<<<< HEAD
                             <h2 class="post-title">Comment and Imppresion</h2>
                             <h3 class="post-subtitle">
                             Messages and impressions from users of our services will really help us in the progress of our services.</h3>
@@ -96,6 +134,15 @@
                             Posted by
                             <a href="#!">Chill Three</a>
                             on May 20, 2022
+=======
+                            <h2 class="post-title">Failure is not an option</h2>
+                            <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Start Bootstrap</a>
+                            on July 8, 2022
+>>>>>>> ebf3df19749f68e9b57feb622a2196a932f61554
                         </p>
                     </div>
                     <!-- Divider-->
